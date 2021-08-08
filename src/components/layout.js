0c72544c2a90d340ee4190e1e6f3eca5523eb1b4
G. Austin Simon <austin@austinsimon.com>
2021-08-08 15:35:13 -0400

    fixed case-sensitive issue

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Preloader from './preloader';
-import UnderDev from './sections/UnderDev';
+import UnderDev from './sections/underdev';
 
 const Layout = () => {
     return(
